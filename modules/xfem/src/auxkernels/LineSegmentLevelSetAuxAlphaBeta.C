432d9882751edaae27bb962c8c1e1b1440031225
borrleo <borrel@wisc.edu>
2018-07-17 12:54:05 -0500

    Use new userobject for Cathcart-Pawel corrosion model

---
+++ modules/xfem/src/auxkernels/LineSegmentLevelSetAuxAlphaBeta.C
@@ -0,0 +1,109 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "LineSegmentLevelSetAuxAlphaBeta.h"
+#include "MovingLineSegmentCutSetUserObjectAlphaBeta.h"
+#include "FEProblem.h"
+#include "MooseError.h"
+
+registerMooseObject("MooseApp", LineSegmentLevelSetAuxAlphaBeta);
+
+template <>
+InputParameters
+validParams<LineSegmentLevelSetAuxAlphaBeta>()
+{
+  InputParameters params = validParams<AuxKernel>();
+  params.addClassDescription(
+      "Auxiliary Kernel that calcuates level set value using line segments' description.");
+  params.addParam<UserObjectName>(
+      "geometric_cut_userobject",
+      "Name of GeometricCutUserObject that gives the line segments information.");
+  return params;
+}
+
+LineSegmentLevelSetAuxAlphaBeta::LineSegmentLevelSetAuxAlphaBeta(const InputParameters & parameters)
+  : AuxKernel(parameters)
+{
+  if (!isNodal())
+    mooseError("LineSegmentLevelSetAuxAlphaBeta: Aux variable must be nodal variable.");
+
+  FEProblemBase * fe_problem = dynamic_cast<FEProblemBase *>(&_subproblem);
+
+  const UserObject * uo =
+      &(fe_problem->getUserObjectBase(getParam<UserObjectName>("geometric_cut_userobject")));
+
+  if (dynamic_cast<const MovingLineSegmentCutSetUserObjectAlphaBeta *>(uo) == nullptr)
+    mooseError("UserObject casting to MovingLineSegmentCutSetUserObjectAlphaBeta in LineSegmentLevelSetAuxAlphaBeta");
+
+  _linesegment_uo = dynamic_cast<const MovingLineSegmentCutSetUserObjectAlphaBeta *>(uo);
+}
+
+Real
+LineSegmentLevelSetAuxAlphaBeta::calculateSignedDistance(Point p)
+{
+  const int line_cut_data_len = 6;
+
+  Real min_dist = std::numeric_limits<Real>::max();
+
+  for (unsigned int i = 0; i < _cut_data.size() / line_cut_data_len; ++i)
+  {
+    Point a = Point(_cut_data[i * line_cut_data_len + 0], _cut_data[i * line_cut_data_len + 1], 0);
+    Point b = Point(_cut_data[i * line_cut_data_len + 2], _cut_data[i * line_cut_data_len + 3], 0);
+
+    Point c = p - a;
+    Point v = (b - a) / (b - a).norm();
+    Real d = (b - a).norm();
+    Real t = v * c;
+
+    Real dist;
+    Point nearest_point;
+
+    if (t < 0)
+    {
+      dist = (p - a).norm();
+      nearest_point = a;
+    }
+    else if (t > d)
+    {
+      dist = (p - b).norm();
+      nearest_point = b;
+    }
+    else
+    {
+      v *= t;
+      dist = (p - a - v).norm();
+      nearest_point = (a + v);
+    }
+
+    Point p_nearest_point = nearest_point - p;
+
+    Point normal_ab = Point(-(b - a)(1), (b - a)(0), 0);
+
+    if (normal_ab * p_nearest_point < 0)
+      dist = -dist;
+
+    if (std::abs(dist) < std::abs(min_dist))
+      min_dist = dist;
+  }
+
+  return min_dist;
+}
+
+void
+LineSegmentLevelSetAuxAlphaBeta::compute()
+{
+  _cut_data = _linesegment_uo->getCutData();
+  AuxKernel::compute();
+}
+
+Real
+LineSegmentLevelSetAuxAlphaBeta::computeValue()
+{
+  return calculateSignedDistance(*_current_node);
+}
---
+++ modules/xfem/src/userobjects/PointValueAtXFEMInterfaceAlphaBeta.C
@@ -0,0 +1,196 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "PointValueAtXFEMInterfaceAlphaBeta.h"
+
+// MOOSE includes
+#include "MooseMesh.h"
+#include "MooseVariableFE.h"
+#include "XFEM.h"
+
+#include "libmesh/mesh_tools.h"
+#include "MovingLineSegmentCutSetUserObjectAlphaBeta.h"
+
+registerMooseObject("XFEMApp", PointValueAtXFEMInterfaceAlphaBeta);
+
+template <>
+InputParameters
+validParams<PointValueAtXFEMInterfaceAlphaBeta>()
+{
+  InputParameters params = validParams<GeneralUserObject>();
+  params.addRequiredCoupledVar(
+      "variable", "The names of the variables that this VectorPostprocessor operates on");
+  params.addParam<UserObjectName>(
+      "geometric_cut_userobject",
+      "Name of GeometricCutUserObject associated with this constraint.");
+  params.addRequiredParam<VariableName>(
+      "level_set_var", "The name of level set variable used to represent the interface");
+  return params;
+}
+
+PointValueAtXFEMInterfaceAlphaBeta::PointValueAtXFEMInterfaceAlphaBeta(const InputParameters & parameters)
+  : GeneralUserObject(parameters),
+    CoupleableMooseVariableDependencyIntermediateInterface(this, false),
+    MooseVariableInterface<Real>(this, false),
+    _mesh(_subproblem.mesh()),
+    _level_set_var_number(
+        _subproblem.getVariable(_tid, parameters.get<VariableName>("level_set_var")).number()),
+    _system(_subproblem.getSystem(getParam<VariableName>("level_set_var"))),
+    _solution(_system.current_local_solution.get())
+{
+  const UserObject * uo =
+      &(_fe_problem.getUserObjectBase(getParam<UserObjectName>("geometric_cut_userobject")));
+
+  if (dynamic_cast<const MovingLineSegmentCutSetUserObjectAlphaBeta *>(uo) == nullptr)
+    mooseError("UserObject casting to GeometricCutUserObject in XFEMSingleVariableConstraint");
+
+  _geo_cut = dynamic_cast<const MovingLineSegmentCutSetUserObjectAlphaBeta *>(uo);
+
+  addMooseVariableDependency(mooseVariable());
+
+  // TODO multiple variables
+  std::vector<std::string> var_names(_coupled_moose_vars.size());
+
+  for (unsigned int i = 0; i < _coupled_moose_vars.size(); i++)
+    var_names[i] = _coupled_moose_vars[i]->name();
+
+  _xfem = MooseSharedNamespace::dynamic_pointer_cast<XFEM>(_fe_problem.getXFEM());
+  if (_xfem == nullptr)
+    mooseError("Problem casting to XFEM in PointValueAtXFEMInterfaceAlphaBeta");
+
+  _elem_pairs = _xfem->getXFEMCutElemPairs(_xfem->getGeometricCutID(_geo_cut));
+
+  std::vector<Real> cut_data = _geo_cut->getCutData();
+
+  for (unsigned int i = 0; i < cut_data.size() / 6; ++i)
+  {
+    _points.push_back(Point(cut_data[i * 6 + 0], cut_data[i * 6 + 1], 0.0));
+    if (i == cut_data.size() / 6 - 1)
+      _points.push_back(Point(cut_data[i * 6 + 2], cut_data[i * 6 + 3], 0.0));
+  }
+}
+
+void
+PointValueAtXFEMInterfaceAlphaBeta::initialize()
+{
+  _pl = _mesh.getPointLocator();
+}
+
+void
+PointValueAtXFEMInterfaceAlphaBeta::execute()
+{
+  _values_positive_level_set_side.clear();
+  _values_negative_level_set_side.clear();
+  _grad_values_positive_level_set_side.clear();
+  _grad_values_negative_level_set_side.clear();
+  _points.clear();
+
+  std::vector<Real> cut_data = _geo_cut->getCutData();
+
+  for (unsigned int i = 0; i < cut_data.size() / 6; ++i)
+  {
+    _points.push_back(Point(cut_data[i * 6 + 0], cut_data[i * 6 + 1]));
+    if (i == cut_data.size() / 6 - 1)
+      _points.push_back(Point(cut_data[i * 6 + 2], cut_data[i * 6 + 3]));
+  }
+
+  BoundingBox bbox = _mesh.getInflatedProcessorBoundingBox();
+
+  std::vector<Point> point_vec(1);
+
+  for (auto i = beginIndex(_points); i < _points.size(); ++i)
+  {
+    Point & p = _points[i];
+
+    if (bbox.contains_point(p))
+    {
+      const Elem * elem = getLocalElemContainingPoint(p, true);
+
+      if (elem)
+      {
+        point_vec[0] = p;
+
+        _subproblem.setCurrentSubdomainID(elem, 0);
+        _subproblem.reinitElemPhys(elem, point_vec, 0);
+
+        _values_positive_level_set_side[i] =
+            (dynamic_cast<MooseVariable *>(_coupled_moose_vars[0]))->sln()[0];
+        _grad_values_positive_level_set_side[i] =
+            ((dynamic_cast<MooseVariable *>(_coupled_moose_vars[0]))->gradSln())[0];
+      }
+
+      const Elem * elem2 = getLocalElemContainingPoint(p, false);
+      if (elem2)
+      {
+        point_vec[0] = p;
+
+        _subproblem.setCurrentSubdomainID(elem2, 0);
+        _subproblem.reinitElemPhys(elem2, point_vec, 0);
+
+        _values_negative_level_set_side[i] =
+            (dynamic_cast<MooseVariable *>(_coupled_moose_vars[0]))->sln()[0];
+        _grad_values_negative_level_set_side[i] =
+            ((dynamic_cast<MooseVariable *>(_coupled_moose_vars[0]))->gradSln())[0];
+      }
+    }
+  }
+}
+
+void
+PointValueAtXFEMInterfaceAlphaBeta::finalize()
+{
+  _communicator.set_union(_values_positive_level_set_side);
+  _communicator.set_union(_grad_values_positive_level_set_side);
+  _communicator.set_union(_values_negative_level_set_side);
+  _communicator.set_union(_grad_values_negative_level_set_side);
+}
+
+const Elem *
+PointValueAtXFEMInterfaceAlphaBeta::getLocalElemContainingPoint(const Point & p, bool positive_level_set)
+{
+  const Elem * elem1 = (*_pl)(p);
+
+  if (elem1->processor_id() != processor_id())
+    return nullptr;
+
+  const Node * node = elem1->node_ptr(0);
+
+  dof_id_type ls_dof_id = node->dof_number(_system.number(), _level_set_var_number, 0);
+
+  Number ls_node_value = (*_solution)(ls_dof_id);
+
+  bool positive = false;
+
+  if (_xfem->isPointInsidePhysicalDomain(elem1, *node))
+  {
+    if (ls_node_value > 0.0)
+      positive = true;
+  }
+  else
+  {
+    if (ls_node_value < 0.0)
+      positive = true;
+  }
+
+  const Elem * elem2;
+  for (auto & pair : *_elem_pairs)
+  {
+    if (pair.first == elem1)
+      elem2 = pair.second;
+    else if (pair.second == elem1)
+      elem2 = pair.first;
+  }
+
+  if ((positive && positive_level_set) || (!positive && !positive_level_set))
+    return elem1;
+  else if ((!positive && positive_level_set) || (positive && !positive_level_set))
+    return elem2;
+  else
+    return nullptr;
+}
